11ae23a398d41e7977be7151b69dc1ac57591c6d
Jerry/Laptop <jerrykeiko98@gmail.com>
2024-02-19 22:39:50 -0800

    Added promptPlayerMove to game().

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-// Game is made up of a board and 2 players
-
 // Game object will host the game
-// gameBoard should have a method to place on it
 const game = () => {
     const gameBoard = gameBoard();
 
     player1 = player('Doug', 'X');
-    player2 = player('Mat', ')');
+    player2 = player('Mat', 'O');
 
+    // Set a player's turn to true
+    player1.updateTurn();
+
+    // Simulate a game
+    // If it is a players turn, get their move
+    const promptPlayerMove = () => {
+        let row, col;
+        if (player1.getTurn() === true) {
+            do {
+                row = prompt(`What row would you like to place ${player1.symbol} in?`);
+                col = prompt(`What column would you like to place ${player1.symbol} in?`);
+            } while (!gameBoard.playerMove(player1.symbol, row, col));
+
+            player1.updateTurn();
+        } else {
+            do {
+                row = prompt(`What row would you like to place ${player2.symbol} in?`);
+                col = prompt(`What column would you like to place ${player2.symbol} in?`);
+            } while (!gameBoard.playerMove(player2.symbol, row, col));
+
+            player2.updateTurn();
+        }
+    }
+
+    return {promptPlayerMove}
 };
 
-
-// Player object creates a player with a name
-// Player can make a choice of where to land on the gameboard
 const player = (name, symbol) => {
     let score = 0;
-    
+    let turn = false;
+
     const getScore = () => (score);
     const addScore = () => (++score);
+    const getTurn = () => (turn);
+    const updateTurn = () => (turn = !turn);
 
-    return {name, symbol, getScore, addScore}
+    return {name, symbol, getScore, addScore, getTurn, updateTurn}
 }
 
 const gameBoard = () => {
@@ -29,12 +51,16 @@ const gameBoard = () => {
         ['', '', '']
     ]
 
-    const place = (symb, row, col) => {
+    const playerMove = (symb, row, col) => {
         if (board[row][col] === ''){
             board[row][col] === symb;
             return true
         } 
          return false 
+    }
+
+    const checkGameOver = () => {
+
     }
 
     const reset = () => board.forEach((row, index) => (board[index] = ['', '', '']));   
@@ -45,5 +71,9 @@ const gameBoard = () => {
     //     ['', '', '']
     // ]);
 
-    return {board, place, reset}
+    return {board, playerMove, reset}
 }
+
+
+const gameInstance = game();
+
