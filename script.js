b3b3a245259f6e3eff7abc565c0d913a1e549deb
Jerry/Laptop <jerrykeiko98@gmail.com>
2024-02-20 18:19:57 -0800

    Updates checkWinner().

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const game = () => {
     player1.updateTurn();
 
     // Simulate a game
+    
+    // Get players move
+
 
     return {promptPlayerMove}
 };
@@ -27,9 +30,9 @@ const player = (name, symbol) => {
 
 const gameBoard = () => {
     let board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+        ['O', 'X', ''],
+        ['O', 'O', 'O'],
+        ['', '', 'O']
     ]
 
     const playerMove = (symb, row, col) => {
@@ -40,9 +43,88 @@ const gameBoard = () => {
          return false 
     }
 
-    const checkGameOver = () => {
+    // Dom element with click event listener that runs playerMove(), then checks for gameOver() after move
 
+
+    const checkWinner = () => {
+        let row1 = board[0];
+        let row2 = board[1];
+        let row3 = board[2];
+        let col1 = [board[0][0], board[1][0], board[2][0]];
+        let col2 = [board[0][1], board[1][1], board[2][1]];
+        let col3 = [board[0][2], board[1][2], board[2][2]];
+        let diagStart = [board[0][0], board[1][1], board[2][2]];
+        let diagEnd = [board[0][2], board[1][1], board[2][0]];
+
+        const winningCombinations = [row1, row2, row3, col1, col2, col3, diagStart, diagEnd];
+
+        for (array of winningCombinations) {
+            let xWinner = true;
+            let oWinner = true;
+
+            for (let x = 0; x < array.length; x++) {
+                if (array[x] !== 'X') {
+                    xWinner = false;
+                } 
+
+                if (array[x] !== 'O') {
+                    oWinner = false;
+                }
+            }
+
+            if (xWinner === true) {
+                return 'X'
+            }
+
+            if (oWinner === true) {
+                return 'O'
+            }
+        }
     }
+
+        // Make these global variables, that get added to everytime a player makes a move, use checkWinner to check these globals for a winner
+
+        // let rowX, rowO;
+        // Check for any 3 in a rows
+        // for (let row = 0; row < board.length; row++) {
+        //     rowX = true;
+        //     rowO = true;
+        //     for (let col = 0; col < board[row].length; col++) {
+        //         // if (board[row][col] === board[row][col+1]){
+        //         //     if (board[row][col] === 'X'){
+        //         //         rowX+=1;
+        //         //     }
+
+        //         //     if (board[row][col] === 'O'){
+        //         //         rowO+=1;
+        //         //     }
+
+        //         // Track rows
+        //         if (board[row][col] !== 'X') {
+        //             rowX = false;
+        //         }
+
+        //         if (board[row][col] !== 'O') {
+        //             rowO = false;
+        //         }
+
+        //         // Track columns
+        //         if (col === 0) {
+        //             col1.push(board[row][col])
+        //         } else if (col === 1) {
+        //             col2.push(board[row][col])
+        //         } else {
+        //             col3.push(board[row][col])
+        //         }
+        //     }
+
+        //     if (rowX) {
+        //         return 'X'
+        //     }
+
+        //     if (rowO) {
+        //         return 'O'
+        //     }
 
     const reset = () => board.forEach((row, index) => (board[index] = ['', '', '']));   
 
@@ -52,9 +134,12 @@ const gameBoard = () => {
     //     ['', '', '']
     // ]);
 
-    return {board, playerMove, reset}
+    return {board, playerMove, checkWinner, reset}
 }
 
 
-const gameInstance = game();
+// const gameInstance = game();
+const board = gameBoard();
+// board.checkWinner();
+console.log(board.checkWinner());
 
