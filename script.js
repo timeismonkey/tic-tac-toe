bdd0ba746df91d0e103788320eb74c21e18077de
jerryDesk <jerrykeiko98@gmail.com>
2024-02-23 16:12:20 -0800

    Updated board ingameBoard() to directly reference the dom elements and playerMove() and checkWinnerAndTie() to check innerHTML.

--- script.js
+++ script.js
@@ -31,27 +31,15 @@ const player = (name, symbol) => {
 const gameBoard = () => {
     let moveCount = 0;
 
-    // let board = [
-    //     ['', 'O', 'O'],
-    //     ['', 'x', ''],
-    //     ['X', 'O', 'X']
-    // ]
-
     let row1 = Array.from(document.querySelectorAll('.row1  li'));
     let row2 = Array.from(document.querySelectorAll('.row2 li'));
     let row3 = Array.from(document.querySelectorAll('.row3 li'));
 
-    row1 = row1.map((item) => item.innerHTML);
-    row2 = row2.map((item) => item.innerHTML);
-    row3 = row3.map((item) => item.innerHTML);
-
     let board = [row1, row2, row3]
 
-
-
     const playerMove = (symb, row, col) => {
-        if (board[row][col] === ''){
-            board[row][col] === symb;
+        if (board[row][col].innerHTML === ''){
+            board[row][col].innerHTML === symb;
             moveCount += 1;
             return true
         } 
@@ -61,22 +49,22 @@ const gameBoard = () => {
     // Dom element with click event listener that runs playerMove(), then checks for gameOver() after move
     const checkWinnerAndTie = () => {
         for (let line = 0; line < board.length; line++) {
-            if ((board[line][0] !== '')  && (board[line][0] === board[line][1]) && (board[line][1] === board[line][2])) {
-                return board[line][0]; 
+            if ((board[line][0].innerHTML !== '')  && (board[line][0].innerHTML === board[line][1].innerHTML) && (board[line][1].innerHTML === board[line][2].innerHTML)) {
+                return board[line][0].innerHTML 
             }
 
-            if (board[0][line] && (board[0][line] === board[1][line]) && (board[1][line] === board[2][line])) {
-                return board[0][line]; 
+            if (board[0][line].innerHTML && (board[0][line].innerHTML === board[1][line].innerHTML) && (board[1][line].innerHTML === board[2][line].innerHTML)) {
+                return board[0][line].innerHTML 
             }
         }
 
         // Check for diagnols 
-        if ((board[0][0] !== '') && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) {
-            return board[0][0]
+        if ((board[0][0].innerHTML !== '') && (board[0][0].innerHTML === board[1][1].innerHTML) && (board[1][1].innerHTML === board[2][2].innerHTML)) {
+            return board[0][0].innerHTML
         }
 
-        if ((board[0][2] !== '') && (board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) ) {
-            return board[0][2]
+        if ((board[0][2].innerHTML !== '') && (board[0][2].innerHTML === board[1][1].innerHTML) && (board[1][1].innerHTML === board[2][0].innerHTML) ) {
+            return board[0][2].innerHTML
         }
 
         // Check tie
@@ -85,7 +73,7 @@ const gameBoard = () => {
         }
     } 
 
-    const reset = () => board.forEach((row, index) => (board[index] = ['', '', '']));   
+    const reset = () => board.forEach((row, index) => (board[index].forEach((item) => item.innerHTML = '')));   
 
     return {board, playerMove, checkWinnerAndTie, reset}
 }
