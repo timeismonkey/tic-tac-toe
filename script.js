4d37754939165d64ab46418cbcf28b6e7e3b494d
jerryDesk <jerrykeiko98@gmail.com>
2024-02-27 20:04:05 -0800

    Working on another iteration of gameBoard().

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const gameController = () => {
     let currentSymbol = 'X';
     let winner;
-    const board = gameBoard.board;
+    const board = gameBoard.getBoard();
 
     let  player1 = player('Doug', 'X');
     let player2 = player('Mat', 'O');
@@ -24,9 +24,112 @@ const player = (name, symbol) => {
     return {name, symbol, getScore, addScore, getTurn, updateTurn}
 }
 
+// const gameBoard = (function() {
+//     let moveCount = 0;
+
+//     // Create an empty gameboard
+//     const board = [];
+//     for (let i = 1; i <= 3; i++) {
+//         const row = [];
+//         for (let j = 1; j <= 3; j++) {
+//             const cell = document.createElement('span');
+//             cell.classList.add('cell');
+//             cell.dataset.row = i;
+//             cell.dataset.col = j;
+//             row.push(cell);
+//         }
+//         board.push(row)
+//     }
+
+//     const getBoard = () => (board);
+
+//     // Log a player's move
+//     const playerMove = (symbol, cell) => {
+//         if (cell.innerHTML === '') {
+//             cell.innerHTML = currentSymbol;
+//             moveCount += 1;
+//             currentSymbol = (currentSymbol === 'X') ? 'O' : 'X';
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+
+//     // Add event listener to each cell in the board
+//     board.forEach((row) => row.forEach((item) => item.addEventListener('click', (e) => {
+//         playerMove(currentSymbol, e.target);
+//         checkWinner();
+//     })))
+
+//     // Dom element with click event listener that runs playerMove(), then checks for gameOver() after move
+//     const checkWinner = () => {
+//         for (let line = 0; line < board.length; line++) {
+//             if ((board[line][0].innerHTML !== '')  && (board[line][0].innerHTML === board[line][1].innerHTML) && (board[line][1].innerHTML === board[line][2].innerHTML)) {
+//                 return board[line][0].innerHTML 
+//             }
+
+//             if (board[0][line].innerHTML && (board[0][line].innerHTML === board[1][line].innerHTML) && (board[1][line].innerHTML === board[2][line].innerHTML)) {
+//                 return board[0][line].innerHTML 
+//             }
+//         }
+
+//         // Check for diagnols 
+//         if ((board[0][0].innerHTML !== '') && (board[0][0].innerHTML === board[1][1].innerHTML) && (board[1][1].innerHTML === board[2][2].innerHTML)) {
+//             return board[0][0].innerHTML
+//         }
+
+//         if ((board[0][2].innerHTML !== '') && (board[0][2].innerHTML === board[1][1].innerHTML) && (board[1][1].innerHTML === board[2][0].innerHTML) ) {
+//             return board[0][2].innerHTML
+//         }
+//     }
+    
+//     const checkTie = () => {
+//         // Check tie
+//         if (moveCount === 9) {
+//             return true
+//         }   
+//     }
+
+//     const reset = () => board.forEach((row, index) => (board[index].forEach((item) => item.innerHTML = '')));   
+
+//     return {getBoard, playerMove, checkWinnerAndTie, reset}
+// })();
+
+const displayController = (function() {
+    let main = document.querySelector('.main')
+    const boardElement = document.createElement('div');
+    boardElement.classList.add('.board');
+    
+    // renderBoard is only called when page is loaded and when a new
+    const renderBoard = () => {
+        // Reference board element
+        const board = gameBoard.getBoard();
+
+        // Clear cu
+        // const boardElement = document.createElement('div');
+        // boardElement.classList.add('.board');
+
+        // Add row elements to boardElement
+        for (let i = 1; i <= 3; i++) {
+            const row = document.createElement('div');
+            row.classList.add(`row`, `row${i}`);
+
+            for (let j = 0; j < board[i-1].length; j++) {
+                row.appendChild(board[i-1][j]);
+            }
+            boardElement.appendChild(row)
+        }
+
+        main.appendChild(boardElement);
+    }
+})();
+
+
+
 const gameBoard = (function() {
     let moveCount = 0;
 
+    // Create an empty gameboard
     const board = [];
     for (let i = 1; i <= 3; i++) {
         const row = [];
@@ -40,22 +143,9 @@ const gameBoard = (function() {
         board.push(row)
     }
 
-    // console.log(board);
-    // let row1 = Array.from(document.querySelectorAll('.row1  span'));
-    // let row2 = Array.from(document.querySelectorAll('.row2 span'));
-    // let row3 = Array.from(document.querySelectorAll('.row3 span'));
+    const getBoard = () => (board);
 
-    // let board = [row1, row2, row3]
-
-    // const playerMove = (symb, row, col) => {
-    //     if (board[row][col].innerHTML === ''){
-    //         board[row][col].innerHTML = symb;
-    //         moveCount += 1;
-    //         return true
-    //     } 
-    //      return false 
-    // }
-
+    // Log a player's move
     const playerMove = (symbol, cell) => {
         if (cell.innerHTML === '') {
             cell.innerHTML = currentSymbol;
@@ -70,11 +160,11 @@ const gameBoard = (function() {
     // Add event listener to each cell in the board
     board.forEach((row) => row.forEach((item) => item.addEventListener('click', (e) => {
         playerMove(currentSymbol, e.target);
-        checkWinnerAndTie();
+        checkWinner();
     })))
 
     // Dom element with click event listener that runs playerMove(), then checks for gameOver() after move
-    const checkWinnerAndTie = () => {
+    const checkWinner = () => {
         for (let line = 0; line < board.length; line++) {
             if ((board[line][0].innerHTML !== '')  && (board[line][0].innerHTML === board[line][1].innerHTML) && (board[line][1].innerHTML === board[line][2].innerHTML)) {
                 return board[line][0].innerHTML 
@@ -93,61 +183,19 @@ const gameBoard = (function() {
         if ((board[0][2].innerHTML !== '') && (board[0][2].innerHTML === board[1][1].innerHTML) && (board[1][1].innerHTML === board[2][0].innerHTML) ) {
             return board[0][2].innerHTML
         }
-
+    }
+    
+    const checkTie = () => {
         // Check tie
         if (moveCount === 9) {
-            return 'tie'
-        }
-    } 
+            return true
+        }   
+    }
 
     const reset = () => board.forEach((row, index) => (board[index].forEach((item) => item.innerHTML = '')));   
 
-    return {board, playerMove, checkWinnerAndTie, reset}
+    return {getBoard, playerMove, checkWinnerAndTie, reset}
 })();
-
-const displayController = (function() {
-    let main = document.querySelector('.main')
-
-    // Create a board element
-    const board = gameBoard.board;
-    const boardElement = document.createElement('div');
-    boardElement.classList.add('.board');
-
-    // Add row elements to boardElement
-    for (let i = 1; i <= 3; i++) {
-        const row = document.createElement('div');
-        row.classList.add(`row`, `row${i}`);
-
-        for (let j = 0; j < board[i-1].length; j++) {
-            row.appendChild(board[i-1][j]);
-        }
-        boardElement.appendChild(row)
-    }
-
-    main.appendChild(boardElement);
-    return {boardElement}
-
-    // Create gameboard element and render it to the page 
-    // const boardContainer = document.createElement('div');
-    // boardContainer.classList.add('board-container')
-
-
-
-    // for (let i = 1; i <= 3; i++) {
-    //     const row = document.createElement('div');
-    //     row.classList.add(`row`, `row${i}`);
-    //     row.dataset.row = i;
-    //     for (let j = 1; j <= 3; j++) {
-    //         const cell = document.createElement('span');
-    //         cell.classList.add('cell');
-    //         cell.dataset.row = i;
-    //         cell.dataset.col = j;
-    //         row.appendChild(cell);
-    //     }
-    //     boardContainer.appendChild(row)
-    // }
-})();
-
 
 // gameBoard();
 
